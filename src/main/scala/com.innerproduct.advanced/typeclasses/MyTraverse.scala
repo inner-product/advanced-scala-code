710005afc058a47286cd342a705463b4bb1097ec
Adam Rosien <adam@rosien.net>
2020-12-01 22:12:29 -0800

    Example IOApp for MyTraverse exercise.

--- src/main/scala/com.innerproduct.advanced/typeclasses/MyTraverse.scala
+++ src/main/scala/com.innerproduct.advanced/typeclasses/MyTraverse.scala
@@ -1,8 +1,11 @@
 package com.innerproduct.advanced.typeclasses
 
 import cats._
+import cats.effect._
+import cats.syntax.all._
+import com.innerproduct.advanced.io.debug._
 
-object MyTraverse {
+object MyTraverse extends IOApp {
   implicit val list: Traverse[List] =
     new Traverse[List] {
       def foldLeft[A, B](fa: List[A], b: B)(f: (B, A) => B): B =
@@ -14,4 +17,10 @@ object MyTraverse {
       def traverse[G[_]: Applicative, A, B](fa: List[A])(f: A => G[B]): G[List[B]] =
         ???
     }
+
+  def run(args: List[String]): IO[ExitCode] =
+    List(1, 2, 3)
+      .traverse(i => IO(i).debug)
+      .debug
+      .as(ExitCode.Success)
 }
