437c76642ac8e89027e230acc59e63f84750c042
Adam Rosien <adam@rosien.net>
2020-12-01 21:10:14 -0800

    Add missing JsArray variant.

--- src/main/scala/com.innerproduct.advanced/typeclasses/Json.scala
+++ src/main/scala/com.innerproduct.advanced/typeclasses/Json.scala
@@ -5,6 +5,7 @@ sealed trait Json
 final case class JsObject(get: Map[String, Json]) extends Json
 final case class JsString(get: String) extends Json
 final case class JsNumber(get: Double) extends Json
+final case class JsArray(get: List[Json]) extends Json
 final case object JsNull extends Json
 
 // The "serialize to JSON" behaviour is encoded in this trait
