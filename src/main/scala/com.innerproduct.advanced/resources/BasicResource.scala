eceede1711dbf058ef520c9d6d2c85696fd22563
Adam Rosien <adam@rosien.net>
2020-12-03 08:09:02 -0800

    Resource examples.

---
+++ src/main/scala/com.innerproduct.advanced/resources/BasicResource.scala
@@ -0,0 +1,18 @@
+package com.innerproduct.advanced.resources
+
+import cats.effect._
+import com.innerproduct.advanced.io.debug._
+
+object BasicResource extends IOApp {
+  def run(args: List[String]): IO[ExitCode] =
+    stringResource
+      .use { s => // <2>
+        IO(s"$s is so cool!").debug
+      }
+      .as(ExitCode.Success)
+
+  val stringResource: Resource[IO, String] = // <1>
+    Resource.make(
+      IO("> acquiring stringResource").debug *> IO("String")
+    )(_ => IO("< releasing stringResource").debug.void)
+}
